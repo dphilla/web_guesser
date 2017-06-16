02d0467b98a2543fb73885bc4dc583ec94380866
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-15 18:02:59 -0600

    sets guess conditions

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-random_number = rand(101)    #placing outside of scope of getter method mean only
+SECRET_NUMBER = rand(101)    #placing outside of scope of getter method mean only
                         # hitting the first call of rand on each refresh
 
 get '/' do
-  erb :index, :locals => {:number => random_number}
+   guess = params['guess']
+   message = check_guess(guess)
+   erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+end
+
+
+
+def check_guess(guess)
+  if params['guess'].to_i > SECRET_NUMBER
+      message = "Too high!"
+  elsif params['guess'].to_i < SECRET_NUMBER
+    message = "Too low!"
+  else
+    message =  "You guessed correctly!" + " The secret number was
+    #{SECRET_NUMBER}"
+  end
 end
