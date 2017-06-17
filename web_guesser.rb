ff5d0ab7a33c32555cdf38b9fd08059f400bdd63
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-16 18:05:02 -0600

    refactor

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
+#what about dealing with inheritance here?
+
 SECRET_NUMBER = rand(101)    #placing outside of scope of getter method mean only
                         # hitting the first call of rand on each refresh
 
@@ -11,15 +13,12 @@ get '/' do
    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
 
-
+# add some kinda loop limiting what is displayed and when?
 
 def check_guess(guess)
-  if params['guess'].to_i > SECRET_NUMBER
-      message = "Too high!"
-  elsif params['guess'].to_i < SECRET_NUMBER
-    message = "Too low!"
-  else
-    message =  "You guessed correctly!" + " The secret number was
-    #{SECRET_NUMBER}"
-  end
+  message = "" if guess.nil?
+  message = "Too high!" if guess.to_i > SECRET_NUMBER
+  message = "Too low!" if guess.to_i < SECRET_NUMBER
+  message =  "Correct! The secret number was #{SECRET_NUMBER}" if guess.to_i == SECRET_NUMBER
+  message
 end
