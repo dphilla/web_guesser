60b33b671856fbe89606844c1dd6b41b8e10924b
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-16 18:42:57 -0600

    sets proper interface

--- web_guesser.rb
+++ web_guesser.rb
@@ -16,9 +16,19 @@ end
 # add some kinda loop limiting what is displayed and when?
 
 def check_guess(guess)
-  message = "" if guess.nil?
-  message = "Too high!" if guess.to_i > SECRET_NUMBER
-  message = "Too low!" if guess.to_i < SECRET_NUMBER
-  message =  "Correct! The secret number was #{SECRET_NUMBER}" if guess.to_i == SECRET_NUMBER
+  message = ""
+  if guess.to_i > SECRET_NUMBER
+    message = "Too high!"
+  elsif  guess != nil && guess.to_i < SECRET_NUMBER
+    message = "Too low!"
+  elsif guess.to_i == SECRET_NUMBER
+    message =  "Correct! The secret number was #{SECRET_NUMBER}"
+  end
+  message
+end
+
+def way_to_high_or_low(guess)
+  message = "Wayy to high!" if (guess.to_i + 5) > SECRET_NUMBER
+  message = "Wayy to low!"  if (guess.to_i - 5) <  SECRET_NUMBER
   message
 end
